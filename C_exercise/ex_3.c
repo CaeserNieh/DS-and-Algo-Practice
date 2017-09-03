28582bdb564159f93636f7befe421842ab091fb1
caesernieh <githubtryemail@gmail.com>
2017-09-04 01:17:23 +0800

    sizeof

---
+++ C_exercise/ex_3.c
@@ -0,0 +1,17 @@
+/*
+sizeof()
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i = 0;
+	printf("%d\n",sizeof(i));
+	scanf("%d",&i);
+	printf("%d",i);
+
+	return 0;
+}
