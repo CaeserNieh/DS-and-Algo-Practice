b7de7a519a6023e4d48ca418c1cdbff2e3ab23a7
CaeserNieh <githubtryemail@gmail.com>
2017-09-05 00:09:53 +0800

    add new commit

--- C_exercise/ex_array_multiple.c
+++ C_exercise/ex_array_multiple.c
@@ -5,7 +5,18 @@
 int main(void){
 	int n;
 	scanf("%d",&n);
-	
+/*
+becase of double array  arr[][]
+we need double pointer 
+at first, we malloc arr1[i]
+according to the integer pointer
+and number of element  and convert void pointer into integer pointer
+to match arr1
+
+after that, we start to malloc each double array.
+we malloc a memory locations according to the sizeof integer
+and number of element
+*/	
 	int **arr1,**arr2,**arr3;
 	arr1 = (int**)malloc(sizeof(int*) * n);
 	arr2 = (int**)malloc(sizeof(int*) * n);
@@ -14,15 +25,15 @@ int main(void){
 	int i,j,k;
 	
 	for(i = 0;i<n;i++){
-		arr1[i] = (int*)malloc(sizeof(int*) * n);
+		arr1[i] = (int*)malloc(sizeof(int) * n);
 	}
 
 	for(i = 0;i<n;i++){
-		arr2[i] = (int*)malloc(sizeof(int*) * n);
+		arr2[i] = (int*)malloc(sizeof(int) * n);
 	}
 
 	for(i = 0;i<n;i++){
-		arr3[i] = (int*)malloc(sizeof(int*) * n);
+		arr3[i] = (int*)malloc(sizeof(int) * n);
 	}
 	
 	for(i = 0;i<n;i++){
